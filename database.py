2959f9a7fdbbe5030c401bb9a424db3d37c4ba21
Scott Houghton <94723197+EdwardHoughton@users.noreply.github.com>
2022-12-06 16:03:45 -0600

    scotts first commit with dan

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:VLyFUBhvfiVcXn3@localhost:3306/serversiderendering"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@localhost:3306/serversiderendering"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
