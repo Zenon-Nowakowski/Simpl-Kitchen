3d221b57cc9ca5fce462a05a39f5510fa6f0e3bf
Daniel jenkins <djenkinsjg0617@gmail.com>
2022-12-09 12:49:50 -0600

    Updated overview html

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def read_item(request: Request, name: schema.Recipe.name, db: Session = Depends(
     ingredients = db.query(Ingredient.name).filter(
         (Ingredient.id == Recipe_Ingredient.ingredient_id) & Recipe_Ingredient.recipe_id == Recipe.id).all()
 
-    return templates.TemplateResponse("newOverview.html", {"request": request, "recipe": item, "data": ingredients})
+    return templates.TemplateResponse("overview.html", {"request": request, "recipe": item, "data": ingredients})
 
 
 # @app.post("/movie/")
