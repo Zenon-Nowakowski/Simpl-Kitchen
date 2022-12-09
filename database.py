b370c0b1138dc34a904e6818c39732dc7fc4b79d
Daniel jenkins <djenkinsjg0617@gmail.com>
2022-12-09 13:08:22 -0600

    Connected app to the remote database

--- database.py
+++ database.py
@@ -3,15 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import mysql.connector as mysql
 
-HOST = "104.237.135.4"
-DATABASE = "simplkitchen"
-USER = "remote"
-PASSWORD = "1234"
+# HOST = "104.237.135.4"
+# DATABASE = "simplkitchen"
+# USER = "remote"
+# PASSWORD = "1234"
 
-db_connection = mysql.connect(
-    host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+# db_connection = mysql.connect(
+#     host=HOST, database=DATABASE, user=USER, password=PASSWORD)
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:VLyFUBhvfiVcXn3@localhost:3306/serversiderendering"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:VLyFUBhvfiVcXn3@localhost:3306/serversiderendering"
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://remote:1234@104.237.135.4:3306/simplkitchen"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
