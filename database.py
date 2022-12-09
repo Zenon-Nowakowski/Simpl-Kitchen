704da06ac75edccfd95c77796595dadd3ee891a1
Daniel jenkins <djenkinsjg0617@gmail.com>
2022-12-09 13:03:39 -0600

    Added new remote database connection

--- database.py
+++ database.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import mysql.connector as mysql
+
+HOST = "104.237.135.4"
+DATABASE = "simplkitchen"
+USER = "remote"
+PASSWORD = "1234"
+
+db_connection = mysql.connect(
+    host=HOST, database=DATABASE, user=USER, password=PASSWORD)
 
 SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:VLyFUBhvfiVcXn3@localhost:3306/serversiderendering"
 
